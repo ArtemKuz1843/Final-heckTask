265d40a55f157a33b555f3e53dac97c93996104e
ArtemKuz <KristofRamuald-1843@yandex.ru>
2023-01-10 14:52:18 +0300

    исправлена ошибка в цикле на длину строки

--- Prog_MasStr-MasStrNeBol_3_Simv/Program.cs
+++ Prog_MasStr-MasStrNeBol_3_Simv/Program.cs
@@ -26,10 +26,10 @@ string[] ResizeStringsArr(string[] arr, int num)
     for (int i = 0; i < arr.GetLength(0); i++)
     {
         string line = arr[i];
-        if ((line.Length - num) > 1)
-        { 
-        line = line.Remove(num, line.Length - num);
-        ResizesArr[i] = line;
+        if ((line.Length - num) > 0)
+        {
+            line = line.Remove(num, line.Length - num);
+            ResizesArr[i] = line;
         }
         else ResizesArr[i] = line;
     }
@@ -39,5 +39,5 @@ string[] ResizeStringsArr(string[] arr, int num)
 Console.WriteLine();
 
 Console.WriteLine("Преобразованный массив строк:");
-string[] result = ResizeStringsArr(Counting, 8);
+string[] result = ResizeStringsArr(Counting, 3);
 PrintArray(result);
